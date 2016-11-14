1accd69f7cdb54a5a77e2ac3f5f22b3a209d5274
chenjianhui <411595429@qq.com>
2016-11-14 15:20:35 +0800

    添加公共标题

--- app/src/main/java/com/nuoman/westernele/home/MainActivity.java
+++ app/src/main/java/com/nuoman/westernele/home/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nuoman.westernele.home;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.nuoman.tabletattendance.R;
 import com.nuoman.westernele.common.BaseActivity;
@@ -8,13 +10,20 @@ import com.nuoman.westernele.common.ICommonAction;
 
 import java.util.Map;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 首页
  */
 public class MainActivity extends BaseActivity implements ICommonAction {
 
+
+    @Bind(R.id.title_left_tv)
+    TextView titleLeftTv;
+    @Bind(R.id.title_right_tv)
+    TextView titleRightTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +37,16 @@ public class MainActivity extends BaseActivity implements ICommonAction {
     @Override
     public void obtainData(Object data, String methodIndex, int status, Map<String, String> parameterMap) {
 
+    }
+
+    @OnClick({R.id.title_left_tv, R.id.title_right_tv})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.title_left_tv:
+                finish();
+                break;
+            case R.id.title_right_tv:
+                break;
+        }
     }
 }
